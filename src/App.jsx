81c1290ab4f7d851a82c37957b9317a059a0f5e8
vitoriacurty <vitoria.curty@hotmail.com>
2023-05-30 20:16:00 -0300

    inicializando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
+import LoginPage from "./pages/LoginPage"
+import CadastroPage from "./pages/CadastroPage"
+import HabitosPage from "./pages/HabitosPage"
+import HojePage from "./pages/HojePage"
+import HistoricoPage from "./pages/HistoricoPage"
 
 export default function App() {
 
   return (
     <>
+    <LoginPage />
+    <CadastroPage />
+    <HabitosPage />
+    <HojePage />
+    <HistoricoPage />
     </>
   )
 }
---
+++ src/pages/CadastroPage/CadastroPage.jsx
@@ -0,0 +1,5 @@
+export default function CadastroPage() {
+    return (
+        <></>
+    )
+}
