efe7b90567ed495b0c093c5da1ad15b77a618b67
vitoriacurty <vitoria.curty@hotmail.com>
2023-05-30 21:13:21 -0300

    add layout da página de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import LoginPage from "./pages/LoginPage"
-import CadastroPage from "./pages/CadastroPage"
-import HabitosPage from "./pages/HabitosPage"
-import HojePage from "./pages/HojePage"
-import HistoricoPage from "./pages/HistoricoPage"
+import LoginPage from "./pages/LoginPage/LoginPage"
+import CadastroPage from "./pages/CadastroPage/CadastroPage"
+import HabitosPage from "./pages/HabitosPage/HabitosPage"
+import HojePage from "./pages/HojePage/HojePage"
+import HistoricoPage from "./pages/HistoricoPage/HistoricoPage"
 
 export default function App() {
 
   return (
     <>
     <LoginPage />
-    <CadastroPage />
+ {/*    <CadastroPage />
     <HabitosPage />
     <HojePage />
-    <HistoricoPage />
+    <HistoricoPage /> */}
     </>
   )
 }
